b8e8898f89a49aaca760d7b406f053e69e3ad588
chrisvarella <chrisvarella@gmail.com>
2020-03-27 18:48:04 -0400

    Update app.py

--- restful/app.py
+++ restful/app.py
@@ -1,21 +1,8 @@
 from flask import Flask, request
 from flask_mysqldb import MySQL
-
-import pymysql
-#from app import app
-#from db_config import mysql
 from flask import jsonify
-#from flask import flash
 
 app = Flask(__name__)
-
-
-#
-#from flask import Flask, render_template, request
-#from flask_mysqldb import MySQL
-#
-#app = Flask(__name__)
-
 
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
